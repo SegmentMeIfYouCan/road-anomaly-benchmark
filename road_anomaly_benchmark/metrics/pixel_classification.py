metrics: fixes to Precision-Recall curve
The pixel classification metrics were originally tested with smoothly distributed values. For example values output by a softmax would range from 0 to 1 and given a usual U-Net we would observe smoothly changing logit levels.

The implementation however creates wrong results in some cases - for instance if we the prediction is binary, score = 0 or 1.

    In dynamic binning mode, set bin edges to surround the whole range of values by adding extra bin edges at minval - eps and maxval + eps. This allows us to correctly treat groups of sharp 0s and 1s scores.

    When calculating PR curve, ensure the curve starts at precision=1,recall=0 and ends at recall=1

    Area under PR curve is calculated according to sklearn and ignores the areas where the curve rises, instead taking a flat section of the graph. (see images)

There is still some mismatch when there are lots of repeated thresholds.

road_anomaly_benchmark/metrics/pixel_classification.py
@@ -29,14 +29,20 @@ def binary_confusion_matrix(
 	if bin_strategy == 'uniform':
 		# bins spread uniforms in 0 .. 1
 		bins = num_bins
-		histogram_range = [0, 1]
+		histogram_range = [-0.01, 1.01]
 
 	elif bin_strategy == 'percentiles':
 		# dynamic bins representing the range of occurring values
 		# bin edges are following the distribution of positive and negative pixels
 
+		# choose thresholds to surround the range of values
+		vmin = np.min(prob)
+		vmax = np.max(prob)
+		vrange = vmax-vmin
+		eps = np.maximum(vrange*1e-2, 1e-2) # make sure there is some separation between the thresholds
+
 		bins = [
-			[0, 1], # make sure 0 and 1 are included
+			[vmin - eps, vmax + eps]
 		]
 
 		if prob_at_true.size:
@@ -174,9 +180,7 @@ class MetricPixelClassification(EvaluationMetric):
 		try:
 			mask_roi = label_pixel_gt < 255
 		except TypeError:
-			print("error: no ground truth available for {}. Please check dataset path...".format(fid))
-			exit()
-
+			raise RuntimeError(f"No ground truth available for {fid}. Please check dataset path...")
 		labels_in_roi = label_pixel_gt[mask_roi]
 		predictions_in_roi = anomaly_p[mask_roi]
 