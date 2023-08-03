metrics: fixes to Precision-Recall curve
The pixel classification metrics were originally tested with smoothly distributed values. For example values output by a softmax would range from 0 to 1 and given a usual U-Net we would observe smoothly changing logit levels.

The implementation however creates wrong results in some cases - for instance if we the prediction is binary, score = 0 or 1.

    In dynamic binning mode, set bin edges to surround the whole range of values by adding extra bin edges at minval - eps and maxval + eps. This allows us to correctly treat groups of sharp 0s and 1s scores.

    When calculating PR curve, ensure the curve starts at precision=1,recall=0 and ends at recall=1

    Area under PR curve is calculated according to sklearn and ignores the areas where the curve rises, instead taking a flat section of the graph. (see images)

There is still some mismatch when there are lots of repeated thresholds.

road_anomaly_benchmark/metrics/pixel_classification_curves.py
@@ -49,45 +49,59 @@ class BinaryClassificationCurve:
 		return cls(**hdf5_read_hierarchy_from_file(path))
 
 
-def get_no_prediction_prefix(cmats):
-	"""
-	The threshold goes from high to low
-	At the beginning, we have 0 predictions and there is no valid precision
-	Remove the prefix with 0 predictions
-
-	This function returns the number of elements to remove from the beginning.
-	"""
-
-	for i in range(cmats.__len__()):
-		if cmats[i, 0, 0] + cmats[i, 0, 1] > 0.01:
-			return i
-
-	raise ValueError('No predictions made at all')
-
-
 def curves_from_cmats(cmats, thresholds):
 	
 	# The threshold goes from high to low
-	# At the beginning, we have 0 predictions and there is no valid precision
-	# Remove the prefix with 0 predictions
 
-	num_remove = get_no_prediction_prefix(cmats)
-
-	if num_remove > 0:
-		print(f'Skip {num_remove}')
-		cmats = cmats[num_remove:]
-		thresholds = thresholds[num_remove:]
 
 	tp = cmats[:, 0, 0]
 	fp = cmats[:, 0, 1]
 	fn = cmats[:, 1, 0]
 	tn = cmats[:, 1, 1]
 
+	num_pos = tp[0] + fn[0]
+	num_neg = fp[0] + tn[0]
+
+	if tp[0] != 0 or fp[0] != 0:
+		tp = np.concatenate([[0], tp])
+		fp = np.concatenate([[0], fp])
+		fn = np.concatenate([[num_pos], fn])
+		tn = np.concatenate([[num_neg], tn])
+
+	if tp[-1] != num_pos or fp[-1] != num_neg:
+		tp = np.concatenate([tp, [num_pos]])
+		fp = np.concatenate([fp, [num_neg]])
+		fn = np.concatenate([fn, [0]])
+		tn = np.concatenate([tn, [0]])
+
+
+	num_steps = tp.shape[0]
+
 	tp_rates = tp / (tp+fn)
 	fp_rates = fp / (fp+tn)
 
-	precisions = tp / (tp+fp)
+	# At a particularly high threshold (at the beginning of the confusion matrix collection)
+	# there may not yet be any predictions (tp+fp == 0).
+	# Calculating precision in that range would result in a division by zero,
+	# we set the precision in that range to 1 to follow how the curve should start.
+
+	n_preds = tp+fp
+	precisions = np.ones(num_steps, np.float64)
+	precisions[n_preds > 0] = tp[n_preds > 0] / n_preds[n_preds > 0]
+
 	recalls = tp / (tp+fn)
+
+	# Ensure the end points of the curve
+	# PR curve starts at precision = 1, recall = 0
+	# if not ( precisions[0] == 1 and recalls[0] == 0 ):
+	# 	precisions = np.concatenate([[1], precisions])
+	# 	recalls = np.concatenate([[0], recalls])
+	# # and ends at recall = 1
+	# if not ( recalls[-1] == 1 ):
+	# 	precisions = np.concatenate([precisions, [0]])
+	# 	recalls = np.concatenate([recalls, [1]])
+
+
 	f1_scores = (2 * tp) / (2 * tp + fp + fn)
 
 	tpr95_index = np.searchsorted(tp_rates, 0.95)
@@ -107,9 +121,13 @@ def curves_from_cmats(cmats, thresholds):
 	best_f1 = f1_scores[ix]
 
 	print(
-		'ap-sum', np.sum(np.diff(recalls) * precisions[:-1]),
+		'ap-sum', np.sum(np.diff(recalls) * precisions[1:]),
 		'ap-trapz', np.trapz(precisions, recalls),
 	)
+
+	# Change the sum strategy to match sklearn rather than 
+	# taking the triangular area under the rising curve.
+	ap = np.sum(np.diff(recalls) * precisions[1:])
 
 	return EasyDict(
 		# curves
@@ -122,7 +140,7 @@ def curves_from_cmats(cmats, thresholds):
 
 		# areas
 		area_ROC = np.trapz(tp_rates, fp_rates),
-		area_PRC = np.trapz(precisions, recalls),
+		area_PRC = ap,
 
 		tpr95_fpr = fpr_tpr95,
 		tpr95_threshold = tpr95_threshold,